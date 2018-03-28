230f75490f2c681f276625b6f3cea3b4cff972f1
Gabriel Pellegrino da Silva <gpsunicamp@unicamp016@gmail.com>
2018-03-28 15:37:00 -0300

    alu final

--- lab4/Project/ex2/alu_board.vhd
+++ lab4/Project/ex2/alu_board.vhd
@@ -14,7 +14,16 @@ architecture behavioral of alu_board is
 	signal a_aux : std_logic_vector(3 downto 0);
 	signal b_aux : std_logic_vector(3 downto 0);
 	signal f_aux : std_logic_vector(3 downto 0);
+	signal a_aux1 : std_logic_vector(3 downto 0);
+	signal b_aux2 : std_logic_vector(3 downto 0);
+	signal f_aux1 : std_Logic_vector(3 downto 0);
 	signal neg_aux: std_logic_vector(2 downto 0);
+	signal HEX4_aux : std_logic_vector(6 downto 0);
+	signal HEX2_aux : std_logic_vector(6 downto 0);
+	signal HEX0_aux : std_logic_vector(6 downto 0);
+	signal HEX4_aux1 : std_logic_vector(6 downto 0);
+	signal HEX2_aux1 : std_logic_vector(6 downto 0);
+	signal HEX0_aux1 : std_logic_vector(6 downto 0);
 	signal oper : std_logic_vector(1 downto 0);
 	signal is_neg: std_logic_vector(6 downto 0);
 	signal not_neg: std_logic_vector(6 downto 0);
@@ -40,13 +49,16 @@ architecture behavioral of alu_board is
   
   end component;
   
+  component bin2hex
+  port (
+	SW: in std_logic_vector(3 downto 0);
+	HEX0: out std_logic_vector(6 downto 0)
+  );
+  
+  end component;
 begin
 
 	oper <= SW(9) & SW(8);
-	--with oper select
-	--	a_aux <= SW(7)&SW(6)&SW(5)&SW(4) when "00" else
-	--				SW(7)&SW(6)&SW(5)&SW(4) when "10" else
-					
 					
   a_aux <= SW(7)&SW(6)&SW(5)&SW(4);
   b_aux <= SW(3)&SW(2)&SW(1)&SW(0);
@@ -71,20 +83,47 @@ begin
   
   my_first: two_comp_to_7seg port map (
 		bin => a_aux,
-		segs => HEX4,
+		segs => HEX4_aux,
 		neg => neg_aux(2)
   );
   my_second: two_comp_to_7seg port map (
 		bin => b_aux,
-		segs => HEX2,
+		segs => HEX2_aux,
 		neg => neg_aux(1)
   );
   my_third: two_comp_to_7seg port map ( 
 		bin => f_aux,
-		segs => HEX0,
+		segs => HEX0_aux,
 		neg => neg_aux(0)
   );
   
+  my_hex_1 : bin2hex port map (
+		SW => a_aux,
+		HEX0 => HEX4_aux1
+  );
+  
+  my_hex_2 : bin2hex port map (
+		SW => b_aux,
+		HEX0 => HEX2_aux1
+  );
+  
+  my_hex_3 : bin2hex port map (
+		SW => f_aux,
+		HEX0 => HEX0_aux1
+  );
+  
+  with oper(0) select
+		HEX4 <= HEX4_aux when '0',
+				  HEX4_aux1 when OTHERS;
+  
+  with oper(0) select
+		HEX2 <= HEX2_aux when '0',
+				  HEX2_aux1 when OTHERS;
+  
+  with oper(0) select
+		HEX0 <= HEX0_aux when '0',
+				  HEX0_aux1 when OTHERS;
+				  
   is_neg <=  "0111111";
   not_neg <= "1111111";
   
@@ -104,5 +143,4 @@ begin
 		HEX1 <= not_neg WHEN '0',
 				  true_neg WHEN OTHERS;
   
--- em operacoes logicas converte para hexadecimal
 end behavioral;
