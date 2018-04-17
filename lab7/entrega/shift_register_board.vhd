c6c88a9257b92a8e81d6ca1b69af221f63c668cb
gpspelle <gpsunicamp016@gmail.com>
2018-04-17 00:31:20 -0300

    noob fi

--- lab7/entrega/shift_register_board.vhd
+++ lab7/entrega/shift_register_board.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity shift_register_board is
 	port (
-		LEDS : out std_logic_vector(5 downto 0);
+		LEDR : out std_logic_vector(5 downto 0);
 		SW : in std_logic_vector (8 downto 0);
-		KEY : in std_logic_vector (0 downto 0);
+		KEY : in std_logic_vector (0 downto 0)
 	);
 end shift_register_board;
 
@@ -24,9 +24,9 @@ begin
 
 	bla: shift_register port map (
 		clk  => KEY(0),
-		mode => SW(7 to 8), 
+		mode => SW(8 downto 7),
 		ser_in => SW(6),
 		par_in => SW(5 downto 0),
-		par_out => LEDS 	
+		par_out => LEDR
 	);
 end structural;
