4d01b0af8ab43a67a4927c4c8625851dc815c64d
gpspelle <gpsunicamp016@gmail.com>
2018-05-08 18:58:51 -0300

    ram_block

--- lab9/project/ex1/ram_block.vhd
+++ lab9/project/ex1/ram_block.vhd
@@ -12,6 +12,24 @@ entity ram_block is
 end ram_block;
 
 architecture direct of ram_block is
+	signal memoria : std_logic_vector(511 downto 0);
 begin
-  -- Your code here!
+	-- todo: Clock eh o nosso Clock?
+	process (Clock)
+	begin
+		if Clock'EVENT and Clock = '1' then
+		-- escritura sincrona com o clock
+			if WrEn = '1' then
+				-- verificando se escrita esta enabled
+				-- todo: arrumar (Address) eh assim que escreve em enderecos de vetor?
+				memoria(Address) <= Data;
+			end if;
+		end if;
+	end process;
+  
+    
+	-- leitura assincrona
+	-- todo: sempre fica lendo assim mesmo?
+	-- todo: arrumar (Address) eh assim que escreve em enderecos de vetor?
+	Q <= memoria(Address);
 end direct;
