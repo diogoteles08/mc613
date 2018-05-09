f9ae143611acc2ed9dca5e66afc92f96fa636be7
gpspelle <gpsunicamp016@gmail.com>
2018-05-09 16:33:31 -0300

    compiling, now test

--- lab9/project/ex1/ram.vhd
+++ lab9/project/ex1/ram.vhd
@@ -32,22 +32,23 @@ port (
 end component;
 
 	signal WrEn_V : std_logic_vector(1 downto 0) := "11";
-	signal true_addr: std_logic_vector(6 downto 0) := "0000000";
+	signal Data_0 : std_logic_vector(31 downto 0);
+	signal Data_1 : std_logic_vector(31 downto 0);
 	
 begin
   ram_b11: ram_block port map (Clock => Clock,
-										  Address => true_addr, 
+										  Address => Address(6 downto 0), 
 										  Data => DataIn(31 downto 24), 
-										  Q => DataOut(31 downto 24), 
+										  Q => Data_0(31 downto 24), 
 										  WrEn => WrEn_V(0)
   );
-  ram_b12: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(23 downto 16), Q => DataOut(23 downto 16), WrEn => WrEn_V(0) );
-  ram_b13: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(15 downto 8), Q => DataOut(15 downto 8), WrEn => WrEn_V(0) );
-  ram_b14: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(7 downto 0), Q => DataOut(7 downto 0), WrEn => WrEn_V(0) );
-  ram_b21: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(31 downto 24), Q => DataOut(31 downto 24), WrEn => WrEn_V(1) );
-  ram_b22: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(23 downto 16), Q => DataOut(23 downto 16), WrEn => WrEn_V(1) );
-  ram_b23: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(15 downto 8), Q => DataOut(15 downto 8), WrEn => WrEn_V(1) );
-  ram_b24: ram_block port map (Clock => Clock, Address => true_addr, Data => DataIn(7 downto 0), Q => DataOut(7 downto 0), WrEn => WrEn_V(1) );
+  ram_b12: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(23 downto 16), Q => Data_0(23 downto 16), WrEn => WrEn_V(0) );
+  ram_b13: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(15 downto 8), Q => Data_0(15 downto 8), WrEn => WrEn_V(0) );
+  ram_b14: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(7 downto 0), Q => Data_0(7 downto 0), WrEn => WrEn_V(0) );
+  ram_b21: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(31 downto 24), Q => Data_1(31 downto 24), WrEn => WrEn_V(1) );
+  ram_b22: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(23 downto 16), Q => Data_1(23 downto 16), WrEn => WrEn_V(1) );
+  ram_b23: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(15 downto 8), Q => Data_1(15 downto 8), WrEn => WrEn_V(1) );
+  ram_b24: ram_block port map (Clock => Clock, Address => Address(6 downto 0), Data => DataIn(7 downto 0), Q => Data_1(7 downto 0), WrEn => WrEn_V(1) );
   
   process (Clock)
   begin 
@@ -58,16 +59,15 @@ begin
 					-- todo: assim ? alta impedancia em DataOut
 					WrEn_v(1 downto 0) <= "00";
 					-- todo: isso aqui acho que vai dar problema
-					DataOut <= (others => '1');
-					true_addr <= "0000000";
+					DataOut <= (others => 'Z');
 				elsif Address(7) = '1' then
 					-- endereco valido de 128 a 255
 					WrEn_v(1 downto 0) <= "10";
-					true_addr <= Address(6 downto 0);
+					DataOut <= Data_1;
 				else
 					-- endereco valido de 0 a 127
 					WrEn_v(1 downto 0) <= "01";
-					true_addr <= Address(6 downto 0);
+					DataOut <= Data_0;
 				end if;
 			end if;
 		end if;
