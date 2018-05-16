3ddcf013f8efa1fbe98f11755bff626662507096
Diogo Teles Sant'Anna <diogoteles08@gmail.com>
2018-05-16 18:13:21 -0300

    Almost finishing bank.vhd

--- lab10/project/Processor/bank.vhd
+++ lab10/project/Processor/bank.vhd
@@ -13,28 +13,24 @@ entity bank is
     DATA_IN : in std_logic_vector(WORDSIZE-1 downto 0);
     DATA_OUT1 : out std_logic_vector(WORDSIZE-1 downto 0);
     DATA_OUT2 : out std_logic_vector(WORDSIZE-1 downto 0)
--- todo: implementar DATA_OUT2
--- todo: implementar RG_ADDR2
--- todo: RD_EN nao ta implementado ainda
   );
 end bank;
 
 architecture structural of bank is
 	component reg 
-		generic (N : INTEGER := 4);
+		generic (N : INTEGER := WORDSIZE);
 		port (
 			clock : in std_logic;
 			DATA_IN : in std_logic_vector(N-1 downto 0);
-			DATA_OUT1 : out std_logic_vector(N-1 downto 0);
+			DATA_OUT : out std_logic_vector(N-1 downto 0);
 			load : in std_logic; --write enable
+			readEn : in std_logic;
 			clear : in std_logic
-                    );
+      );
 	end component;
 
--- todo: pou preciso da sua ajuda, esses generics tem que ir pra 5? e arrumar o
--- resto do codigo que muda junto com isso
 	component zbuffer 
-		generic (N : INTEGER := 4);
+		generic (N : INTEGER := WORDSIZE);
 		port (
 			enable: in std_logic;
 			Xin: in std_logic_vector(N-1 downto 0);
@@ -42,45 +38,77 @@ architecture structural of bank is
 		);
 	end component;
 
-	component dec3_to_8
+	component dec5_to_32
 		port (
 			en: in std_logic;
-			Xin: in std_logic_vector(2 downto 0);
-			Xout: out std_logic_vector(7 downto 0)
+			Xin: in std_logic_vector(4 downto 0);
+			Xout: out std_logic_vector(31 downto 0)
 		);
 	end component;
 	
-	signal D2R: std_logic_vector(7 downto 0);
-	signal R2D: std_logic_vector(7 downto 0);
-	signal output_vector: std_logic_vector (8*4 downto 0);
+	signal writeAddressDecoded: std_logic_vector(31 downto 0);
+	signal readAddress1Decoded: std_logic_vector(31 downto 0);
+	signal readAddress2Decoded: std_logic_vector(31 downto 0);
+	signal output_vector1: std_logic_vector (WORDSIZE*32 downto 0);
+	signal output_vector2: std_logic_vector (WORDSIZE*32 downto 0);
 begin
 	
-        inputSelection: dec3_to_8 port map (
+   inputSelection: dec5_to_32 port map (
 		en => '1',
 		Xin => WR_ADDR,
-		Xout => D2R
+		Xout => writeAddressDecoded
 	);	
-	outputSelection: dec3_to_8 port map (
+	
+	outputSelection1: dec5_to_32 port map (
 		en => '1',
 		Xin => RG_ADDR1,
-		Xout => R2D
+		Xout => readAddress1Decoded
 	);
 	
-	create_regs: for i in 0 to 7 generate
+	outputSelection2: dec5_to_32 port map (
+		en => '1',
+		Xin => RG_ADDR2,
+		Xout => readAddress2Decoded
+	);
+	
+	
+	
+
+	create_regs: for i in 1 to 31 generate
 		regis: reg port map (
 			clock => clock,
 			DATA_IN => DATA_IN,
-			DATA_OUT1 => output_vector(i*4+3 downto i*4),
-			load => WR_EN and D2R(i),
+			DATA_OUT => output_vector1(i*4+31 downto i*4),
+			load => WR_EN and writeAddressDecoded(i),
+			readEn => RD_EN and readAddress1Decoded(i),
+			clear => clear
+		);
+	end generate;
+	
+	create_regs: for i in 1 to 31 generate
+		regis: reg port map (
+			clock => clock,
+			DATA_IN => open,
+			DATA_OUT => output_vector2(i*4+31 downto i*4),
+			load => '0',
+			readEn => RD_EN and readAddress2Decoded(i),
 			clear => clear
 		);
 	end generate;
 	
 	filter_output: for i in 0 to 7 generate
 		buffer_t_state: zbuffer port map (
-			enable => R2D(i),
-			Xin => output_vector(i*4+3 downto i*4),
+			enable => readAddress1Decoded(i),
+			Xin => output_vector1(i*4+3 downto i*4),
 			Xout => DATA_OUT1
+		);
+	end generate;
+	
+	filter_output: for i in 0 to 7 generate
+		buffer_t_state: zbuffer port map (
+			enable => readAddress2Decoded(i),
+			Xin => output_vector2(i*4+3 downto i*4),
+			Xout => DATA_OUT2
 		);
 	end generate;
 end structural;
