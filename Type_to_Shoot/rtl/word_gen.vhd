9620806cc9f99e2859e9695e2cfa98a3eb7f9428
gpspelle <gpsunicamp016@gmail.com>
2018-05-17 14:43:13 -0300

    besteirinhas

---
+++ Type_to_Shoot/rtl/word_gen.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity word_gen is
+  port (
+    Clock : in std_logic;
+ 
+  );
+end word_gen;
+
+architecture rtl of word_gen is
+
+begin
+
+end rtl;
