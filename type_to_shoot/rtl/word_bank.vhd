bee8da39365bf606c7bda7e93e3604b3152d53d4
Diogo Teles Sant'Anna <diogoteles08@gmail.com>
2018-06-05 21:51:46 -0300

    Tested and fixed word_bank

--- type_to_shoot/rtl/word_bank.vhd
+++ type_to_shoot/rtl/word_bank.vhd
@@ -15,31 +15,33 @@ entity word_bank is
 end word_bank;
 
 architecture rtl of word_bank is
-	signal words_aux: word_table;
-	signal num_words_aux: integer := 0;
 begin
+	-- insercao e remocao podem ser feitos ao mesmo tempo, soh da erro quando
+	-- o numero de palavras jah eh o maximo e se tenta excluir e inserir ao mesmo tempo
 	process(clock)
+		variable words_aux: word_table;
+		variable num_words_aux: integer := 0;
 	begin
 		if clock'event and clock = '1' then
 			if insert_new_word = '1' then
-				words_aux(num_words_aux) <= new_word;
-				num_words_aux <= num_words_aux + 1;
+				words_aux(num_words_aux) := new_word;
+				num_words_aux := num_words_aux + 1;
 			end if;
 		
 			if kill_word = '1' then
 				-- Does the shift
 				for i in 0 to max_words-2 loop
 					if i >= word_to_kill_index and i <= num_words_aux-2 then
-						words_aux(i) <= words_aux(i+1);
+						words_aux(i) := words_aux(i+1);
 					end if;
 				end loop;
-				
-				num_words_aux <= num_words_aux - 1;
-				words_aux(num_words_aux) <= no_word; -- Limpa o espaco no fim da lista
+
+				num_words_aux := num_words_aux - 1;
+				words_aux(num_words_aux) := no_word; -- Limpa o espaco no fim da lista
 			end if;		
 		end if;
+
+		words <= words_aux;
+		num_words <= num_words_aux;
 	end process;
-	
-	words <= words_aux;
-	num_words <= num_words_aux;
 end rtl;
